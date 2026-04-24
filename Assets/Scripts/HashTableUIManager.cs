913fc6ca43602f234a44decd1f7f94c0a04053e5
KimSeongsoo <rlatjdtn620@gmail.com>
2026-04-24 09:55:46 +0900

    ADD

---
+++ Assets/Scripts/HashTableUIManager.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HashTableUIManager : MonoBehaviour
+{
+    public TMP_InputField keyInput;
+    public TMP_InputField valueInput;
+
+    public Button addButton;
+    public Button removeButton;
+    public Button clearButton;
+
+
+}
