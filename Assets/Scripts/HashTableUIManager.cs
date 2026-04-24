826f23e103e3524aa87e839a3d99c59d14855d12
KimSeongsoo <rlatjdtn620@gmail.com>
2026-04-24 11:45:07 +0900

    Ui 셋업 로그뷰 및 드롭다운 프리펩 설정

--- Assets/Scripts/HashTableUIManager.cs
+++ Assets/Scripts/HashTableUIManager.cs
@@ -11,5 +11,30 @@ public class HashTableUIManager : MonoBehaviour
     public Button removeButton;
     public Button clearButton;
 
+    public LogViewScripts logView;
 
+    private void Awake()
+    {
+        addButton.onClick.AddListener(OnAddClick);
+        removeButton.onClick.AddListener(OnRemoveClick);
+        clearButton.onClick.AddListener(OnClearClick);
+    }
+
+    public void Start()
+    {
+        Debug.Log("해시테이블 시작");
+    }
+    private void OnAddClick()
+    {
+        Debug.Log("키 밸류 ADD");
+    }
+
+    private void OnRemoveClick()
+    {
+        Debug.Log("키 밸류 REMOVE");
+    }
+    private void OnClearClick()
+    {
+        Debug.Log("Clear : 모든 항목 삭제됨");
+    }
 }
--- Assets/Scripts/LogViewScripts.cs
+++ Assets/Scripts/LogViewScripts.cs
@@ -1,16 +1,42 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogViewScripts : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TextMeshProUGUI logTextPrefab;
+    public Transform content;
+    public ScrollRect scrollRect;
+
+    private void OnEnable()
     {
-        
+        Application.logMessageReceived += HandleLog;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string message, string stackTrace, LogType type)
+    {
+        AddLog(message);
+    }
+
+    public void AddLog(string message)
+    {
+        TextMeshProUGUI logItem = Instantiate(logTextPrefab, content);
+        logItem.text = message;
+
+        Canvas.ForceUpdateCanvases();
+        scrollRect.verticalNormalizedPosition = 0f;
+    }
+
+    public void ClearLog()
+    {
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
